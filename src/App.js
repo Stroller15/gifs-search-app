1256424555f0cc421f2eb2eb82968c3c7f2f185c
Your Name <shubhamverma6351@gmail.com>
2023-07-27 17:16:27 +0530

    feat: render data on web page
    fetched data from api and render through app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,122 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import NavBar from "components/NavBar/NavBar";
+import Spinner from "components/Spinner/Spinner";
+import GifRoute from "components/GifRoute";
+import "./App.css";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = "https://api.giphy.com/v1/gifs/";
 
 const App = () => {
+  // State variables
+  const [giphyData, setGiphyData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Fetch data on mount
+  useEffect(() => {
+    const fetchGifs = async () => {
+      setIsError(false);
+
+      try {
+        let endpoint = "";
+
+        endpoint = `${API_URL}trending?api_key=${API_KEY}&limit=25&offset=0&rating=Y&lang=en`;
+
+        const results = await axios.get(endpoint);
+        setGiphyData(results.data.data);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+      }
+    };
+
+    fetchGifs();
+  }, [searchTerm]);
+
+  // Handle search input and submit
+  const handleSearch = async () => {
+    setIsError(false);
+    setIsLoading(true);
+
+    try {
+      const endpoint = `${API_URL}search?api_key=${API_KEY}&q=${searchTerm}&_limit=25&rating=Y&lang=en`;
+
+      const results = await axios.get(endpoint);
+
+      setGiphyData(results.data.data);
+    } catch (error) {
+      console.error(error);
+      setIsError("Oops! Something went wrong. Please try again later.");
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    handleSearch();
+  };
+
+  const handleClick = async () => {
+    handleSearch();
+  };
+
+  const loadMoreData = async () => {
+    try {
+      let endpoint = "";
+
+      if (searchTerm) {
+        endpoint = `${API_URL}search?q=${searchTerm}&api_key=${API_KEY}&limit=25&offset=${
+          currentPage * 25
+        }&rating=Y&lang=en`;
+      } else {
+        endpoint = `${API_URL}trending?api_key=${API_KEY}&limit=25&offset=${
+          currentPage * 25
+        }&rating=Y&lang=en`;
+      }
+
+      const results = await axios.get(endpoint);
+      const newData = results.data.data;
+
+      setGiphyData((prevData) => [...prevData, ...newData]);
+      setCurrentPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      setGiphyData([]);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+      setIsError(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello, React!</h1>
+    <div className="giphy-container">
+      <NavBar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        handleSubmit={handleSubmit}
+        handleClick={handleClick}
+      />
+
+      {isLoading ? (
+        <Spinner />
+      ) : isError ? (
+        <div className="error-container">
+          <p className="error-message">
+            Oops! Something went wrong. Please try again later.
+          </p>
+        </div>
+      ) : (
+        <GifRoute
+          giphyData={giphyData}
+          loadMoreData={loadMoreData}
+         
+        />
+      )}
     </div>
   );
 };
