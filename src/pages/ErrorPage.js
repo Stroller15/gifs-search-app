df3dd21ff3c4164e6613cdacd235b00224978bde
Your Name <shubhamverma6351@gmail.com>
2023-07-27 17:11:11 +0530

    feat: :sparkles: implemented error handling

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "24px",
+        fontSize: "24px",
+        color: "white",
+        fontWeight: "bold",
+      }}
+    >
+      404 Error: Page Not Found.
+    </div>
+  );
+};
+
+export default ErrorPage;
